5d525cc2d5f8cb78f4437be75617ef8118c94a5f
Joe <ujbeszel@gmail.com>
2017-10-08 20:43:13 +0200

    Fix typo

--- public/class-exopite-multifilter-public.php
+++ public/class-exopite-multifilter-public.php
@@ -237,7 +237,7 @@ class Exopite_Multifilter_Public {
         }
 
         $ret .= '</div>';
-        $ret .= '</figcation>';
+        $ret .= '</figcaption>';
         $ret .= '</figure>';
         $ret .= '</a>';
 
